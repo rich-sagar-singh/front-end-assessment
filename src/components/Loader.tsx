fc494cc870bf888f050d3c9166c90ebc90089a49
rich-sagar-singh <richsagarsingh@gmail.com>
2025-02-04 18:43:31 +0500

    Add files via upload

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Loader() {
+  return (
+    <img
+      src="/loadingAnimation.gif"
+      alt="Running Puppy"
+      className="w-full h-full object-contain"
+    />
+  );
+}
